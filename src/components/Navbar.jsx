c24630bd167c07d3a39ca9b1ffa12511a62b32cb
Sunny Vinay <sunnyvinay7@gmail.com>
2024-08-07 17:21:15 -0700

    top navigation bar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from "../assets/logo.png";
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <nav className='mb-20 flex items-center justify-between py-6'>
+        <div className="flex flex-shrink-0 items-center">
+            <img className="mx-2 w-10" src={logo} alt="logo"/>
+        </div>
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+            <FaLinkedin />
+            <FaGithub />
+        </div> 
+    </nav>
+  )
+}
+
+export default Navbar;
