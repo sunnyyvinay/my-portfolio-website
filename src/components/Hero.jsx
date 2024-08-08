c1771282e73739cc29efedcc46c58ffbfd872868
Sunny Vinay <sunnyvinay7@gmail.com>
2024-08-08 14:08:31 -0700

    add animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,23 +1,55 @@
 import React from 'react';
 import { HERO_CONTENT } from '../constants';
 import profilePic from "../assets/profilepic.png";
+import { motion } from "framer-motion";
 
+const container = (delay) => ({
+    hidden : {x: -100, opacity: 0},
+    visible: {
+        x: 0,
+        opacity: 1,
+        transition: { duration: 0.5, delay: delay },
+    }
+ })
 const Hero = () => {
   return (
     <div className="borber-b border-neutral-900 pb-4 lg:mb-35">
         <div className="flex flex-wrap">
             <div className='w-full lg:w-1/2'>
                 <div className='flex flex-col items-center lg:items-start'>
-                    <h1 className='pb-16 text-6xl font-thin tracking-tight lg-:mt-16 lg:text-8xl'>Sunny Vinay</h1>
-                    <span className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>
-                        Full Stack + AI/ML Developer
-                    </span>
-                    <p className="my-2 max-w-xl py-6 font-light tracking-tighter">{HERO_CONTENT}</p>
+                    <motion.h1 
+                        className='pb-16 text-6xl font-thin tracking-tight lg-:mt-16 lg:text-8xl'
+                        variants={container(0)}
+                        initial="hidden"
+                        animate="visible">
+                            Sunny Vinay
+                    </motion.h1>
+
+                    <motion.span 
+                        className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'
+                        variants={container(0.5)}
+                        initial="hidden"
+                        animate="visible">
+                            Full Stack + AI/ML Developer
+                    </motion.span>
+
+                    <motion.p 
+                        className="my-2 max-w-xl py-6 font-light tracking-tighter"
+                        variants={container(1)}
+                        initial="hidden"
+                        animate="visible">
+                            {HERO_CONTENT}
+                    </motion.p>
                 </div>
             </div>
+            
             <div className='w-full lg:w-1/2 lg:p-8'>
                 <div className='flex justify-center'>
-                    <img src={profilePic} alt="Profile Picture" />
+                    <motion.img 
+                        initial={{x: 100, opacity: 0}} 
+                        animate={{x: 0, opacity: 1}}
+                        transition={{duration: 1, delay: 1.2}}
+                        src={profilePic} alt="Profile Picture" />
                 </div>
             </div>
         </div>
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,28 +1,77 @@
-import React from 'react'
-import { RiReactjsLine } from 'react-icons/ri'
+import React from 'react';
+import { RiReactjsLine } from 'react-icons/ri';
+import { motion } from "framer-motion";
+
+const iconVariants = (duration) => ({
+    initial: {y: -10},
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse"
+        }
+    }
+})
 
 // use 'text-<color>-400' to replace colors of icons
 const Technologies = () => {
   return (
     <div className='border-b border-neutral-800 pb-24'>
-        <h2 className='my-20 text-center text-4xl'>Technologies</h2>
-        <div className='flex flex-wrap items-center justify-center gap-4'>
-            <div className='rounded-2xl border-4 border-entural-800 p-4'>
-                <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-entural-800 p-4'>
-                <RiReactjsLine className='text-7xl'/> 
-            </div>
-            <div className='rounded-2xl border-4 border-entural-800 p-4'>
-                <RiReactjsLine className='text-7xl text-green-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-entural-800 p-4'>
-                <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </div>
-            <div className='rounded-2xl border-4 border-entural-800 p-4'>
-                <RiReactjsLine className='text-7xl text-cyan-400'/>
-            </div>
-        </div>
+        <motion.h2 
+            whileInView={{opacity: 1, y: 0}}
+            initial={{opacity: 0, y: -100}}
+            transition={{duration: 1.5}}
+            className='my-20 text-center text-4xl'>
+                Technologies
+        </motion.h2>
+
+        <motion.div 
+            whileInView={{opacity: 1, x: 0}}
+            initial={{opacity: 0, x: -100}}
+            transition={{duration: 1.5}}
+            className='flex flex-wrap items-center justify-center gap-4'>
+                <motion.div 
+                    variants={iconVariants(2.5)}
+                    initial="initial"
+                    animate="animate"
+                    className='rounded-2xl border-4 border-entural-800 p-4'>
+                        <RiReactjsLine className='text-7xl text-cyan-400'/>
+                </motion.div>
+
+            <motion.div 
+                variants={iconVariants(3)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-entural-800 p-4'>
+                    <RiReactjsLine className='text-7xl'/> 
+            </motion.div>
+
+            <motion.div 
+                variants={iconVariants(5)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-entural-800 p-4'>
+                    <RiReactjsLine className='text-7xl text-green-400'/>
+            </motion.div>
+
+            <motion.div 
+                variants={iconVariants(2)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-entural-800 p-4'>
+                    <RiReactjsLine className='text-7xl text-cyan-400'/>
+            </motion.div>
+
+            <motion.div 
+                variants={iconVariants(6)}
+                initial="initial"
+                animate="animate"
+                className='rounded-2xl border-4 border-entural-800 p-4'>
+                    <RiReactjsLine className='text-7xl text-cyan-400'/>
+            </motion.div>
+        </motion.div>
     </div>
   )
 }
