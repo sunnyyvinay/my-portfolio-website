63877bd94171fdfd48b4ef95ac8ac9b3f73d2600
Sunny Vinay <sunnyvinay7@gmail.com>
2024-09-17 11:43:02 -0700

    resize profilepic

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -49,6 +49,7 @@ const Hero = () => {
                         initial={{x: 100, opacity: 0}} 
                         animate={{x: 0, opacity: 1}}
                         transition={{duration: 1, delay: 1.2}}
+                        width={400} height={400}
                         src={profilePic} alt="Profile Picture"/>
                 </div>
             </div>
